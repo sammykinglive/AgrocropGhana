1ec75d0fcf66fd883bf91a8758ade91027d38555
Samuel Lartey <86780084+sammykinglive@users.noreply.github.com>
2023-07-08 21:15:55 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+// JavaScript for the slideshow
+const slideshowImages = document.querySelectorAll('.slideshow-image');
+let currentImageIndex = 0;
+
+function showNextImage() {
+  const currentImage = slideshowImages[currentImageIndex];
+  currentImage.classList.remove('active');
+
+  currentImageIndex = (currentImageIndex + 1) % slideshowImages.length;
+  const nextImage = slideshowImages[currentImageIndex];
+  nextImage.classList.add('active');
+}
+
+setInterval(showNextImage, 5000); // Change image every 3 seconds
+
+
+
+
+
+
+ // Create a new Intersection Observer
+ var observer = new IntersectionObserver(function(entries) {
+  entries.forEach(function(entry) {
+    if (entry.isIntersecting) {
+      animateIcons();
+    }
+  });
+});
+
+// Observe the target element
+observer.observe(document.querySelector('.about-icons'));
+
+function animateIcons() {
+  $('.icon').each(function(index) {
+    var animation = 'animate__fadeInUp'; // Choose the animation class from Animate.css
+    var delay = index * 200; // Delay between animations for each icon
+
+    $(this).addClass('animate__animated ' + animation);
+    $(this).css('animation-delay', delay + 'ms');
+  });
+}
+
+
+
+
+
+var observer = new IntersectionObserver(function(entries) {
+  entries.forEach(function(entry) {
+    if (entry.isIntersecting) {
+      animateItems(entry.target);
+    }
+  });
+});
+
+$('.grid-item').each(function() {
+  observer.observe(this);
+});
+
+function animateItems(target) {
+  $(target)
+    .find('.product-card')
+    .each(function(index) {
+      var animation = 'animate__fadeInUp'; // Choose the animation class from Animate.css
+      var delay = index * 200; // Delay between animations for each card
+
+      $(this).addClass('animate__animated ' + animation);
+      $(this).css('animation-delay', delay + 'ms');
+    });
+}
+
+
+
+
+//scroll effect for product section
+
+
+
+
+
+
+
+
+
+
